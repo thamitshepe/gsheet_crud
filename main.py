f6c34a29515d3e9d04206c108328fc95db3f3b9c
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-10-23 11:32:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,40 @@ file = gspread.authorize(creds)
 workbook = file.open("Inventory")
 sheet = workbook.sheet1
 
+def size_to_string(size):
+    # Function to ensure size is always a string
+    return str(size)
+
 @app.post("/edit-shoe")
 async def edit_shoe(
     shoe_name: str,
     sku: str,
-    size: str,
+    size: str,  # Now "size" is a parameter that you set
     new_shoe_name: typing.Optional[str] = Query(None, title="Optional: New Shoe Name"),
     new_sku: typing.Optional[str] = Query(None, title="Optional: New SKU"),
     new_cost: typing.Optional[float] = Query(None, title="Optional: New Cost"),
-    new_size: typing.Optional[str] = Query(None, title="Optional: New Size"),
     new_quantity: typing.Optional[int] = Query(None, title="Optional: New Quantity"),
     new_list_price: typing.Optional[float] = Query(None, title="Optional: New List Price"),
     new_condition: typing.Optional[str] = Query(None, title="Optional: New Condition")
 ):
-
-    # Debugging: Log the received size parameter
-    print(f"Received size parameter: '{size}'")
 
     # Find all rows matching the specified "Shoe" and "SKU"
     all_rows = sheet.get_all_records()
     rows_to_update = []
 
     for index, row in enumerate(all_rows, start=2):
-        if row.get("Shoe") == shoe_name and row.get("Sku") == sku and row.get("Size") == size:
+        if row.get("Shoe") == shoe_name and row.get("Sku") == sku:
+            # Use the "Size" value from the Google Sheets column
+            size_from_sheets = size_to_string(row.get("Size"))
+
+            # Debugging: Log the received size parameter
+            print(f"Received size parameter: '{size}' (from the request)")
+            print(f"Size value from Google Sheets: '{size_from_sheets}'")
+
+            # Check if the size from the request matches the size from the Google Sheets
+            if size != size_from_sheets:
+                return {"message": "Size in the request does not match the Size from Google Sheets."}
+
             # Update the columns based on the provided values
             if new_shoe_name is not None:
                 row["Shoe"] = new_shoe_name
@@ -47,25 +58,22 @@ async def edit_shoe(
                 row["Sku"] = new_sku
             if new_cost is not None:
                 row["Cost"] = new_cost
-            if new_size is not None:
-                row["Size"] = new_size
             if new_quantity is not None:
                 row["Quantity"] = new_quantity
             if new_list_price is not None:
                 row["List Price"] = new_list_price
             if new_condition is not None:
                 row["Condition"] = new_condition
-            rows_to_update.append((index, list(row.values())))
+            rows_to_update.append((index, list(row.values()), size_from_sheets))
 
     if not rows_to_update:
         return {"message": "Shoe, SKU, and Size combination not found"}
 
-    # Prepare the values for updating the specified columns
-    for index, columns in rows_to_update:
+    for index, columns, size in rows_to_update:
         # Update the columns with the modified values
         # Calculate the range based on the number of columns in the data
         range_start = f"A{index}"
         range_end = chr(ord("A") + len(columns) - 1) + str(index)
         sheet.update(range_start + ":" + range_end, [columns], value_input_option="RAW")
 
-    return {"message": "Cells updated"}
+    return {"message": "Cells updated", "size": size}
