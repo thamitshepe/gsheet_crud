e1e588991911db9a2621f4118c068ecb3b522467
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-10-23 11:55:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def size_to_string(size):
 async def edit_shoe(
     shoe_name: str,
     sku: str,
-    size: str,
+    size: str: typing.Optional[str] = Query(None, title="Optional: Size"),
     new_size: typing.Optional[str] = Query(None, title="Optional: New Size"),
     new_shoe_name: typing.Optional[str] = Query(None, title="Optional: New Shoe Name"),
     new_sku: typing.Optional[str] = Query(None, title="Optional: New SKU"),
@@ -35,23 +35,23 @@ async def edit_shoe(
     new_condition: typing.Optional[str] = Query(None, title="Optional: New Condition")
 ):
 
-    # Find the row matching the specified "Shoe," "SKU," and "Size"
+    # Find the row matching the specified "Shoe," "SKU," and optionally "Size"
     all_rows = sheet.get_all_records()
     row_to_update = None
 
     for index, row in enumerate(all_rows, start=2):
-        if row.get("Shoe") == shoe_name and row.get("Sku") == sku and row.get("Size"):
-            # Use the "Size" value from the Google Sheets column
-            size_from_sheets = size_to_string(row.get("Size"))
+        if row.get("Shoe") == shoe_name and row.get("Sku") == sku:
+            if size:  # If "size" is provided in the request, consider it
+                if row.get("Size"):
+                    size_from_sheets = size_to_string(row.get("Size"))
+                    if size != size_from_sheets:
+                        continue
+                else:
+                    continue
 
-            # Debugging: Log the received size parameter
-            print(f"Received size parameter from the request: '{size}'")
-            print(f"Size value from Google Sheets: '{size_from_sheets}'")
-
-            # Check if the size from the request matches the size from the Google Sheets
-            if size == size_from_sheets:
-                row_to_update = row
-                break
+            # If it reaches this point, it means it matched Shoe, SKU, and Size (if specified)
+            row_to_update = row
+            break
 
     if not row_to_update:
         return {"message": "Shoe, SKU, and Size combination not found"}
