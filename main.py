a966a8261a5d35f887581443b55f1dd17e351ecc
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-10-23 08:01:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ sheet = workbook.sheet1
 
 @app.post("/edit-shoe")
 async def edit_shoe(
-    shoe_name: str, sku: str, new_shoe_name: str = None, new_sku: str = None,
-    new_cost: float = None, new_size: str = None, size: str = None, new_quantity: int = None, quantity: int = None,
-    new_list_price: float = None, list_price: float = None, condition: str = None, new_condition: str = None
+    shoe_name: str, sku: str, size: str, quantity: int, list_price: float, condition: str, new_shoe_name: str = None, new_sku: str = None,
+    new_cost: float = None, new_size: str = None, new_quantity: int = None,
+    new_list_price: float = None, new_condition: str = None
 ):
 
     # Find all rows matching the specified "Shoe" and "SKU"
@@ -37,18 +37,22 @@ async def edit_shoe(
             if new_cost is not None:
                 row["Cost"] = new_cost
             if new_size is not None and row.get("Size") == size:
-                # Update the size only when the size matches the specified condition
                 row["Size"] = new_size
+            elif new_size is not None:
+                return {"message": "Size not found"}
             if new_quantity is not None and row.get("Size") == size and row.get("Quantity") == quantity:
-                # Update the quantity when both size and quantity match the conditions
                 row["Quantity"] = new_quantity
+            elif new_quantity is not None:
+                return {"message": "Size and Quantity combination not found"}
             if new_list_price is not None and row.get("Size") == size and row.get("List Price") == list_price:
-                # Update the list price when size and list price match the conditions
                 row["List Price"] = new_list_price
+            elif new_list_price is not None:
+                return {"message": "Size and List Price combination not found"}
             if new_condition is not None and row.get("Size") == size and row.get("Condition") == condition:
-                # Update the condition when size and condition match the conditions
                 row["Condition"] = new_condition
-            rows_to_update.append((index, list(row.values())))
+            elif new_condition is not None:
+                return {"message": "Size and Condition combination not found"}
+            rows_to_update.append((index, list(row.values()))
 
     if not rows_to_update:
         return {"message": "Shoe and SKU combination not found"}
