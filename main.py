4a3da61249ff5a00b5871b30aebe438fe27777ee
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-10-25 14:28:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,19 +17,6 @@ file = gspread.authorize(creds)
 workbook = file.open("Inventory")
 sheet = workbook.sheet1  # Define the specific sheet in your workbook
 
-# Define a dictionary to map field names to column names
-field_to_column = {
-    "Shoe": "Shoe",
-    "Sku": "Sku",
-    "Cost": "Cost",
-    "Size": "Size",
-    "Complete": "Complete",
-    "Source": "Source",
-    "Seller": "Seller",
-    "Note": "Note",
-    "Date": "Date"
-}
-
 def size_to_string(size):
     # Function to ensure size is always a string
     return str(size)
@@ -67,48 +54,7 @@ async def edit_shoe(
     # Ensure that sku is always treated as a string
     sku = sku_to_string(sku)
 
-    # Find the first row (header row) of the sheet to get the column titles
-    header_row = sheet.row_values(1)
-
-    # Construct the new row
-    new_row = ["" for _ in range(len(header_row))]  # Initialize with empty strings
-
-    for field_name, field_value in {
-        "Shoe": shoe_name,
-        "Sku": sku,
-        # Map other fields to their corresponding keys
-        "Cost": cost,
-        "Size": add_size,
-        "Complete": complete,
-        "Source": cur_source,
-        "Seller": cur_seller,
-        "Note": cur_note,
-        "Date": date
-    }.items():
-        if field_value is not None:
-            # Find the column name corresponding to the field name
-            column_name = field_to_column.get(field_name)
-            if column_name is not None:
-                # Find the index of the matching column title
-                column_index = header_row.index(column_name)
-                new_row[column_index] = field_value
-
-    # Insert the new row if "add_size" and "cost" are provided
-    if add_size is not None and cost is not None:
-        # Find the last row with the same SKU
-        last_row_index = None
-        for index, row in enumerate(sheet.get_all_records(), start=2):
-            if sku_to_string(row.get("Sku")) == sku:
-                last_row_index = index
-
-        # If a matching row is found, insert the new row immediately after it
-        if last_row_index is not None:
-            sheet.insert_rows([new_row], last_row_index + 1)
-            return {"message": "New size added"}
-
-    return {"message": "add_size and cost are required for the operation"}
-
-    # Rest of the code for updating and deleting rows
+# updating and deleting rows
     all_rows = sheet.get_all_records()
     rows_to_update = []
 
