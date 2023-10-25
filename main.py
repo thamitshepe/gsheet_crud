fc72d1889793d9dcb19077f04cb5ad91b383e82d
Thami Tshepe <60922877+thamitshepe@users.noreply.github.com>
2023-10-25 20:48:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,89 +47,87 @@ async def edit_shoe(
     # Ensure that sku is always treated as a string
     sku = sku_to_string(sku)
 
-# Find all the rows matching the specified "Shoe," "SKU," and optionally "Size"
-all_rows = sheet.get_all_records()
-rows_to_update = []
+    # Find all the rows matching the specified "Shoe," "SKU," and optionally "Size"
+    all_rows = sheet.get_all_records()
+    rows_to_update = []
 
-for index, row in enumerate(all_rows, start=2):
-    if row.get("Shoe") == shoe_name:
-        if sku is not None and row.get("Sku") == sku:
-            if size is not None:
-                if row.get("Size") and size_to_string(row.get("Size")) == size:
-                    # If it reaches this point, it means it matched Shoe, SKU, and Size (if specified)
-                    rows_to_update.append((index, row))
-                elif not row.get("Size") and size is None:
-                    # If "Size" is not specified in both request and row, consider it a match
-                    rows_to_update.append((index, row))
-        elif sku is None:
-            # When "SKU" is not specified, match rows based on "Shoe" and "Size" if provided
-            if size is not None:
-                if row.get("Size") and size_to_string(row.get("Size")) == size:
-                    rows_to_update.append((index, row))
-                elif not row.get("Size"):
-                    # If "Size" is not specified in both request and row, consider it a match
-                    rows_to_update.append((index, row))
+    for index, row in enumerate(all_rows, start=2):
+        if row.get("Shoe") == shoe_name:
+            if sku is not None and row.get("Sku") == sku:
+                if size is not None:
+                    if row.get("Size") and size_to_string(row.get("Size")) == size:
+                        # If it reaches this point, it means it matched Shoe, SKU, and Size (if specified)
+                        rows_to_update.append((index, row))
+                    elif not row.get("Size") and size is None:
+                        # If "Size" is not specified in both request and row, consider it a match
+                        rows_to_update.append((index, row))
+            elif sku is None:
+                # When "SKU" is not specified, match rows based on "Shoe" and "Size" if provided
+                if size is not None:
+                    if row.get("Size") and size_to_string(row.get("Size")) == size:
+                        rows_to_update.append((index, row))
+                    elif not row.get("Size"):
+                        # If "Size" is not specified in both request and row, consider it a match
+                        rows_to_update.append((index, row))
 
+    if delete:
+        rows_to_delete = []
+        if size:
+            # Delete specific SKU and Size combination
+            rows_to_delete = [index for index, row in enumerate(all_rows, start=2)
+                              if sku_to_string(row.get("Sku")) == sku and size_to_string(row.get("Size")) == size]
+        else:
+            # Delete all rows with a specific SKU
+            rows_to_delete = [index for index, row in enumerate(all_rows, start=2)
+                              if sku_to_string(row.get("Sku")) == sku]
 
-if delete:
-    rows_to_delete = []
-    if size:
-        # Delete specific SKU and Size combination
-        rows_to_delete = [index for index, row in enumerate(all_rows, start=2)
-                          if sku_to_string(row.get("Sku")) == sku and size_to_string(row.get("Size")) == size]
-    else:
-        # Delete all rows with a specific SKU
-        rows_to_delete = [index for index, row in enumerate(all_rows, start=2)
-                          if sku_to_string(row.get("Sku")) == sku]
+        if not rows_to_delete:
+            return {"message": "No rows found for deletion"}  # This 'return' is now inside the 'if delete' block
 
-    if not rows_to_delete:
-        return {"message": "No rows found for deletion"}  # This 'return' is now inside the 'if delete' block
+        # Sort rows in descending order so that rows can be deleted without shifting indices
+        rows_to_delete.sort(reverse=True)
 
-    # Sort rows in descending order so that rows can be deleted without shifting indices
-    rows_to_delete.sort(reverse=True)
+        for index in rows_to_delete:
+            sheet.delete_rows(index)
 
-    for index in rows_to_delete:
-        sheet.delete_rows(index)
+        return {"message": f"{len(rows_to_delete)} rows deleted"}
 
-    return {"message": f"{len(rows_to_delete)} rows deleted"}
+    if not rows_to_update:
+        return {"message": "Shoe, SKU, and Size combination not found"}
 
-if not rows_to_update:
-    return {"message": "Shoe, SKU, and Size combination not found"}
+    for index, row in rows_to_update:
+        # Update the columns based on the provided values
+        if new_size is not None:
+            row["Size"] = new_size
+        if new_shoe_name is not None:
+            row["Shoe"] = new_shoe_name
+        if new_sku is not None:
+            row["Sku"] = sku_to_string(new_sku)  # Ensure new_sku is treated as a string
+        if new_cost is not None:
+            row["Cost"] = new_cost
+        if new_quantity is not None:
+            row["Quantity"] = new_quantity
+        if new_condition is not None:
+            row["Condition"] = new_condition
+        if new_list_price is not None:
+            row["List Price"] = new_list_price
+        if listed is not True:
+            row["Listed"] = True
+        else:
+            row["Listed"] = False
+        if source is not None:
+            row["Source"] = source
+        if seller is not None:
+            row["Seller"] = seller
+        if note is not None:
+            row["Note"] = note
 
-for index, row in rows_to_update:
-    # Update the columns based on the provided values
-    if new_size is not None:
-        row["Size"] = new_size
-    if new_shoe_name is not None:
-        row["Shoe"] = new_shoe_name
-    if new_sku is not None:
-        row["Sku"] = sku_to_string(new_sku)  # Ensure new_sku is treated as a string
-    if new_cost is not None:
-        row["Cost"] = new_cost
-    if new_quantity is not None:
-        row["Quantity"] = new_quantity
-    if new_condition is not None:
-        row["Condition"] = new_condition
-    if new_list_price is not None:
-        row["List Price"] = new_list_price
-    if listed is not True:
-        row["Listed"] = True
-    else:
-        row["Listed"] = False
-    if source is not None:
-        row["Source"] = source
-    if seller is not None:
-        row["Seller"] = seller
-    if note is not None:
-        row["Note"] = note
+        # Calculate the range for the specific row
+        range_start = f"A{index}"
+        range_end = chr(ord("A") + len(row) - 1) + str(index)
+        sheet.update(range_start + ":" + range_end, [list(row.values())], value_input_option="RAW")
 
-    # Calculate the range for the specific row
-    range_start = f"A{index}"
-    range_end = chr(ord("A") + len(row) - 1) + str(index)
-    sheet.update(range_start + ":" + range_end, [list(row.values())], value_input_option="RAW")
-
-return {"message": "Cells updated"}  # This 'return' is outside the 'for' loop
-
+    return {"message": "Cells updated"}  # This 'return' is outside the 'for' loop
 
 @app.post("/add-size")
 async def add_size(
@@ -151,7 +149,7 @@ async def add_size(
     all_records = sheet.get_all_records()
     last_row_index = None
 
-    for index, row in enumerate(all_records, start=2):
+    for index, row in enumerate all_records, start=2):
         if sku_to_string(row.get("Sku")) == sku:
             last_row_index = index
 
